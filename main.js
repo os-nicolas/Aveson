279a192b6badd0422599c40ea61cf4fc7b3b8381
os-nicolas <Colin.Wielga@ncf.edu>
2015-12-09 15:00:03 -0500

    here is some stuff

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $("#test").click(function () {
+        console.log("clicked");
+        $(".option1").animate({ 'left': '200px' }, 500);
+        $(".option2").animate({ 'left': '1000px' }, 500);
+        $(".option3").animate({ 'left': '1000px' }, 500);
+        setTimeout(function () {
+            $(".option1").animate({
+                width: '900px',
+                'left': '0px'
+            });
+        }, 500)
+    });
+
+});
