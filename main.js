85757d2c1dec16d5777598c1dc34755d59d472ae
os-nicolas <Colin.Wielga@ncf.edu>
2015-12-09 15:40:38 -0500

    pow pow pow

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ $(document).ready(function () {
         $(".option2").animate({ left: '1000px' }, 500);
         $(".option3").animate({ left: '1000px' }, 500);
         setTimeout(function () {
+            //$(".option2").hide();
+            //$(".option3").hide();
             $(".option1").animate({
                 width: '900px',
+                left: '300px'
             });
             $(".window").animate({
                 "margin-left": '-300px',
