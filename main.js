acb2fcf3b1e2d67caec4365bb09eb703f8b4ec95
Deepwater Creations <dylan.craine@deepwatercreations.com>
2015-12-09 15:35:47 -0700

    Made options directly clickable, took away test button

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 $(document).ready(function () {
-    $("#test").click(function () {
-        console.log("clicked");
-        $(".option1").animate({ left: '200px' }, 500);
-        $(".option2").animate({ left: '1000px' }, 500);
-        $(".option3").animate({ left: '1000px' }, 500);
+    var NUM_OPTIONS = 3;
+    
+    $(".option").click(function(event){
+	var clickedOpt = $(event.currentTarget);
+	var position = parseInt(clickedOpt.attr("position"));
+
+	console.log("clicked " + position);
+
+	//Move the unclicked options out of the way
+	var optionList = $(".option-list > .option");
+	optionList.each(function(idx, opt) {
+	    var distance = idx === position ? "200px" : "1000px";
+            $(opt).animate({left: distance}, 500);
+	})
+
         setTimeout(function () {
             //$(".option2").hide();
             //$(".option3").hide();
-            $(".option1").animate({
+            clickedOpt.animate({
                 width: '900px',
                 left: '300px'
             });
