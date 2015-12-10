bcf7b3e6aa2e2488f0c5abb66bd2741efd795aba
Deepwater Creations <dylan.craine@deepwatercreations.com>
2015-12-10 13:08:21 -0700

    fixed indentation - tab is 4 spaces

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
 $(document).ready(function () {
     var NUM_OPTIONS = 3;
-    
+
     //Click event for the small, unopened option
     $(".option").click(function(event){
-	var clickedOpt = $(event.currentTarget);
-	var position = parseInt(clickedOpt.attr("position"));
-	var optionList = $(".option-list > .option");
+        var clickedOpt = $(event.currentTarget);
+        var position = parseInt(clickedOpt.attr("position"));
+        var optionList = $(".option-list > .option");
 
-	console.log("clicked " + position);
+        console.log("clicked " + position);
 
-	if(clickedOpt.attr("state") === "minimized"){
-	//Move the unclicked options out of the way
-	    optionList.each(function(idx, opt) {
-		var distance = 1000;
-		if(idx !== position){
-		    //Options to the left of the clicked option should fly away to the left,
-		    //options to the right should fly right.
-		    //We find the difference between their indices and divide by the absolute value
-		    //to get positive or negative, and multiply the distance by that. 
-		    var rel_idx = (idx - position);
-		    var sign = rel_idx / Math.abs(rel_idx);
-		    distance *= sign; 
-		}
-		else{
-		    //We can get the "centered" location for the clicked option
-		    //by subtracting one and multiplying by the centered left side position.
-		    distance = (idx - 1) * -200; 		
-		}
-		distance += "px";
+        if(clickedOpt.attr("state") === "minimized"){
+            //Move the unclicked options out of the way
+            optionList.each(function(idx, opt) {
+                var distance = 1000;
+                if(idx !== position){
+                    //Options to the left of the clicked option should fly away to the left,
+                    //options to the right should fly right.
+                    //We find the difference between their indices and divide by the absolute value
+                    //to get positive or negative, and multiply the distance by that. 
+                    var rel_idx = (idx - position);
+                    var sign = rel_idx / Math.abs(rel_idx);
+                    distance *= sign; 
+                }
+                else{
+                    //We can get the "centered" location for the clicked option
+                    //by subtracting one and multiplying by the centered left side position.
+                    distance = (idx - 1) * -200; 		
+                }
+                distance += "px";
                 $(opt).animate({left: distance}, 500);
-	    })
+            })
 
             setTimeout(function () {
                 //$(".option2").hide();
@@ -43,27 +43,27 @@ $(document).ready(function () {
                 });
             }, 500)
 
-	    clickedOpt.attr("state", "maximized");
-	}
-	else{
-	    //Animate minimizing the clicked option
-	        $(".window").animate({
-		    "margin-left": '',
-	        });
-	        clickedOpt.animate({
-		    width: '300px',
-		    left: ''
-	        });
+            clickedOpt.attr("state", "maximized");
+        }
+        else{
+            //Animate minimizing the clicked option
+            $(".window").animate({
+                "margin-left": '',
+            });
+            clickedOpt.animate({
+                width: '300px',
+                left: ''
+            });
 
-	    //Bring back the other options
-	    setTimeout(function(){
-	    optionList.each(function(idx, opt) {
-                $(opt).animate({left: "0px"}, 500);
-	    })
-	    }, 500);
+            //Bring back the other options
+            setTimeout(function(){
+                optionList.each(function(idx, opt) {
+                    $(opt).animate({left: "0px"}, 500);
+                })
+            }, 500);
 
-	    clickedOpt.attr("state", "minimized");
-	}
+            clickedOpt.attr("state", "minimized");
+        }
     });
-	
+
 });
